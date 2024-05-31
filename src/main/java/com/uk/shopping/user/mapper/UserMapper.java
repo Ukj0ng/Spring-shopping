270b00b78a6667cd30f29c0417ba9e0a6a8a8994
Ukjong <xb253q@gmail.com>
2024-06-01 02:24:28 +0900

    feat: User 엔티티와 DTO를 변환할 UserMapper 추 - JoinRequestDto -> User

--- src/main/java/com/uk/shopping/user/mapper/UserMapper.java
+++ src/main/java/com/uk/shopping/user/mapper/UserMapper.java
@@ -1,2 +1,10 @@
-package com.uk.shopping.user.mapper;public interface UserMapper {
+package com.uk.shopping.user.mapper;
+
+import com.uk.shopping.user.dto.JoinRequestDto;
+import com.uk.shopping.user.model.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    User toEntity(JoinRequestDto joinRequestDto);
 }
