c48556c5f9b5a1808a83d4df020fa8fc46ddc59f
Ukjong <xb253q@gmail.com>
2024-06-01 02:59:07 +0900

    feat: User의 비밀번호 해쉬화

--- src/main/java/com/uk/shopping/user/service/UserService.java
+++ src/main/java/com/uk/shopping/user/service/UserService.java
@@ -1,11 +1,14 @@
 package com.uk.shopping.user.service;
 
 import com.uk.shopping.user.dto.JoinRequestDto;
+import com.uk.shopping.user.dto.LoginRequestDto;
 import com.uk.shopping.user.mapper.UserMapper;
 import com.uk.shopping.user.model.User;
 import com.uk.shopping.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,16 +16,19 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     // 생성자를 통해 의존성 주입
-    public UserService(UserRepository userRepository, UserMapper userMapper) {
+    public UserService(UserRepository userRepository, UserMapper userMapper, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void join(JoinRequestDto joinRequestDto) {
         User user = userMapper.toEntity(joinRequestDto);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 }
