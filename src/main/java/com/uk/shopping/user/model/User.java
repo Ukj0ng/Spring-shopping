a892da3635faba446cec2cb75ee8df9696bcae1c
Ukjong <xb253q@gmail.com>
2024-05-31 19:04:27 +0900

    feat: User 엔티티 - id - username - password - email - name - createTime - updateTime - enabled - role

--- src/main/java/com/uk/shopping/user/model/User.java
+++ src/main/java/com/uk/shopping/user/model/User.java
@@ -1,2 +1,46 @@
-package com.uk.shopping.user.model;public class User {
+package com.uk.shopping.user.model;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.sql.Timestamp;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 30, unique = true)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 10)
+    private String email;
+
+    @Column(nullable = false, length = 10)
+    private String name;
+
+    @CreationTimestamp
+    private Timestamp createTime;
+
+    @UpdateTimestamp
+    private Timestamp updateTime;
+
+    @Column(nullable = false)
+    private boolean enabled;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType role;
 }
