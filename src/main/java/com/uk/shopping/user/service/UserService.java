e17eea72e30b2689415699bcb8768cddf5caea97
Ukjong <xb253q@gmail.com>
2024-06-01 02:26:55 +0900

    feat: User의 비즈니스 로직을 처리할 UserService 추가 - join

--- src/main/java/com/uk/shopping/user/service/UserService.java
+++ src/main/java/com/uk/shopping/user/service/UserService.java
@@ -1,14 +1,28 @@
 package com.uk.shopping.user.service;
 
 import com.uk.shopping.user.dto.JoinRequestDto;
+import com.uk.shopping.user.mapper.UserMapper;
+import com.uk.shopping.user.model.User;
+import com.uk.shopping.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    // 생성자를 통해 의존성 주입
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
     @Transactional
     public void join(JoinRequestDto joinRequestDto) {
-        System.out.println("UserService join실행");
+        User user = userMapper.toEntity(joinRequestDto);
+        userRepository.save(user);
     }
 }
