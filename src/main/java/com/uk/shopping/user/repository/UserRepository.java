f28702e1b2e18e2d0d8fd2b02be75b43b8d42bb4
Ukjong <xb253q@gmail.com>
2024-06-01 02:22:43 +0900

    feat: User 엔티티를 위한 UserRepository DAO 추가

--- src/main/java/com/uk/shopping/user/repository/UserRepository.java
+++ src/main/java/com/uk/shopping/user/repository/UserRepository.java
@@ -1,7 +1,7 @@
-package com.uk.shopping.user;
+package com.uk.shopping.user.repository;
 
 import com.uk.shopping.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
