0323cc5337f86a02637495c983cac8d14cc5095f
Ukjong <xb253q@gmail.com>
2024-06-01 02:19:59 +0900

    feat: 사용자의 회원가입 요청을 받을 DTO - username - password - name - email

--- src/main/java/com/uk/shopping/user/dto/JoinRequestDto.java
+++ src/main/java/com/uk/shopping/user/dto/JoinRequestDto.java
@@ -9,11 +9,11 @@ import lombok.Data;
 public class JoinRequestDto {
 
     @NotBlank(message = "사용자 이름은 필수 항목입니다.")
-    @Size(min = 4, max = 30, message = "사용자 이름은 4자 이상 30자 이하로 설정해야 합니다.")
+    @Size(min = 2, max = 30, message = "사용자 이름은 4자 이상 30자 이하로 설정해야 합니다.")
     private String username;
 
     @NotBlank(message = "비밀번호는 필수 항목입니다.")
-    @Size(min = 8, max = 100, message = "비밀번호는 8자 이상 100자 이하로 설정해야 합니다.")
+    @Size(min = 4, max = 100, message = "비밀번호는 8자 이상 100자 이하로 설정해야 합니다.")
     private String password;
 
     @NotBlank(message = "이름은 필수 항목입니다.")
