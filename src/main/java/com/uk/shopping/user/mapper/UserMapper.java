2b3115ed7b4febe1b0ee0cd424ab2689c717938b
Ukjong <xb253q@gmail.com>
2024-06-02 17:59:10 +0900

    feat: @Mapping 어노테이션 추가 - id mapping 제외 - createTime mapping 제외 - updateTime mapping 제외 - enabled mapping 제외

--- src/main/java/com/uk/shopping/user/mapper/UserMapper.java
+++ src/main/java/com/uk/shopping/user/mapper/UserMapper.java
@@ -3,8 +3,13 @@ package com.uk.shopping.user.mapper;
 import com.uk.shopping.user.dto.JoinRequestDto;
 import com.uk.shopping.user.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createTime", ignore = true)
+    @Mapping(target = "updateTime", ignore = true)
+    @Mapping(target = "enabled", ignore = true)
     User toEntity(JoinRequestDto joinRequestDto);
 }
