70f4684e71b87c1b60398e1bff4c4d4d081ddbdf
Ukjong <xb253q@gmail.com>
2024-06-02 18:01:30 +0900

    feat: 예외 처리시 출력할 에러 메세지 - Inavalid username or password - User not found - Invalid user

--- src/main/java/com/uk/shopping/common/constants/ErrorMessage.java
+++ src/main/java/com/uk/shopping/common/constants/ErrorMessage.java
@@ -1,2 +1,18 @@
-package com.uk.shopping.common.constants;public enum ErrorMessage {
+package com.uk.shopping.common.constants;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorMessage {
+    INVALID_USERNAME_OR_PASSWORD("Invalid username or password"),
+    USER_NOT_FOUND("User not found"),
+    INVALID_USER("Invalid user");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
 }
+
+
