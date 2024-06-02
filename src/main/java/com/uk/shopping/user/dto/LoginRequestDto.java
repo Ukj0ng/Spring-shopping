64bc3a29d736c37eaa9fba77b3fa55d5319f43ff
Ukjong <xb253q@gmail.com>
2024-06-02 17:55:59 +0900

    feat: 사용자의 로그인 요청을 받을 DTO - username - password

--- src/main/java/com/uk/shopping/user/dto/LoginRequestDto.java
+++ src/main/java/com/uk/shopping/user/dto/LoginRequestDto.java
@@ -1,2 +1,14 @@
-package com.uk.shopping.user.dto;public class LoginRequestDto {
+package com.uk.shopping.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequestDto {
+
+    @NotBlank(message = "사용자 이름은 필수 항목입니다.")
+    private String username;
+
+    @NotBlank(message = "비밀번호는 필수 항목입니다.")
+    private String password;
 }
