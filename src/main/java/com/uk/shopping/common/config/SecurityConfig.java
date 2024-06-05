2b18300a41d35aa80752d607361c6980c861df15
Ukjong <xb253q@gmail.com>
2024-06-05 17:13:18 +0900

    feat: filterChain에 필터 추가 - JwtAuthenticationFilter - JwtAuthorizationFilter - session disable - httpBasic disable

--- src/main/java/com/uk/shopping/common/config/SecurityConfig.java
+++ src/main/java/com/uk/shopping/common/config/SecurityConfig.java
@@ -1,15 +1,25 @@
 package com.uk.shopping.common.config;
 
+import com.uk.shopping.common.auth.PrincipalDetailsService;
+import com.uk.shopping.common.jwt.JWTAuthorizationFilter;
+import com.uk.shopping.common.jwt.JwtAuthenticationFilter;
+import com.uk.shopping.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final UserRepository userRepository;
     // Bean을 붙히면 해당 메서드의 리턴되는 오브젝트를 IoC에 등록해준다.
     @Bean
     public BCryptPasswordEncoder encoder() {
@@ -17,12 +27,30 @@ public class SecurityConfig {
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) throws Exception {
+        AuthenticationManagerBuilder builder = httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        builder.userDetailsService(new PrincipalDetailsService(userRepository))
+            .passwordEncoder(encoder());
+        return builder.build();
+    }
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        AuthenticationManager authenticationManager = authenticationManager(http);
+
         http
+            .authenticationManager(authenticationManager)
             .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests(authorize -> authorize
+            .sessionManagement(sessionManagement -> sessionManagement
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .addFilter(new JwtAuthenticationFilter(authenticationManager))
+            .addFilter(new JWTAuthorizationFilter(authenticationManager, userRepository))
+            .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll()
             );
 
-    return http.build();}
+        return http.build();
+    }
 }
