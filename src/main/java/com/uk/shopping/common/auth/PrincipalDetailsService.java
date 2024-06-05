d185c8cf4b732c5250133ed59d56bd499958f98c
Ukjong <xb253q@gmail.com>
2024-06-05 17:08:03 +0900

    feat: PrincipalDetailsService 클래스 추가 및 UserDetailsService 인터페이스 구현

--- src/main/java/com/uk/shopping/common/auth/PrincipalDetailsService.java
+++ src/main/java/com/uk/shopping/common/auth/PrincipalDetailsService.java
@@ -1,2 +1,23 @@
-package com.uk.shopping.common.auth;public class PrincipalDetailsService {
+package com.uk.shopping.common.auth;
+
+import com.uk.shopping.user.model.User;
+import com.uk.shopping.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("Principal details Service의: " + username);
+        User user = userRepository.findByUsername(username);
+        return new PrincipalDetails(user);
+    }
 }
