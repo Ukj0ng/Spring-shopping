c5602f5dc1a706d2abd71163b27f2f0ba971f206
Ukjong <xb253q@gmail.com>
2024-06-05 17:10:55 +0900

    feat: JwtAuthenticationFilter 클래스 추 - JWT 기반 인증 필터 구현

--- src/main/java/com/uk/shopping/common/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/uk/shopping/common/jwt/JwtAuthenticationFilter.java
@@ -1,2 +1,66 @@
-package com.uk.shopping.common.jwt;public class JwtAuthenticationFilter {
+package com.uk.shopping.common.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uk.shopping.common.auth.PrincipalDetails;
+import com.uk.shopping.user.model.User;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+        HttpServletResponse response) throws AuthenticationException {
+        try {
+            System.out.println("로그인 실행중");
+            ObjectMapper objectMapper = new ObjectMapper();
+            User user = objectMapper.readValue(request.getInputStream(), User.class);
+            System.out.println("user: " + user.toString());
+
+            UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
+
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            System.out.println("authentication: " + authentication);
+            PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+
+            System.out.println("PrincipalDetails: " + principalDetails.getUsername());
+            System.out.println("PrincipalDetails: " + principalDetails.getUser().getUsername());
+            return authentication;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+        HttpServletResponse response, FilterChain chain, Authentication authResult)
+        throws IOException, ServletException {
+        System.out.println("successfulAuthentication 실행, 인증완료");
+        PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+
+        String jwtToken = JWT.create()
+                .withSubject(principalDetails.getUser().getUsername())
+                    .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
+                        .withClaim("id", principalDetails.getUser().getId())
+                            .withClaim("username", principalDetails.getUser().getUsername())
+                                .sign(Algorithm.HMAC256(JwtProperties.SECRET));
+
+        response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
+        System.out.println(jwtToken);
+    }
 }
