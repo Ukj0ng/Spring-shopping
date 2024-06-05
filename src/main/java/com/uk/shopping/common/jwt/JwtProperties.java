d20095af30731698b5e3539a472becb27c2c9268
Ukjong <xb253q@gmail.com>
2024-06-05 17:28:12 +0900

    feat: JWTProperties 인터페이스 구현

--- src/main/java/com/uk/shopping/common/jwt/JwtProperties.java
+++ src/main/java/com/uk/shopping/common/jwt/JwtProperties.java
@@ -1,2 +1,8 @@
-package com.uk.shopping.common.jwt;public interface JwtProperties {
+package com.uk.shopping.common.jwt;
+
+public interface JwtProperties {
+    String SECRET = "secretKey";
+    int EXPIRATION_TIME = 60000 * 30;
+    String TOKEN_PREFIX = "Bearer ";
+    String HEADER_STRING = "Authorization";
 }
