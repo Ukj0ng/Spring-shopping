5cc2f443de18402111987285e938cc3df4d8212f
Ukjong <xb253q@gmail.com>
2024-06-05 17:05:59 +0900

    feat: PrincipalDetails 클래스 추가 및 UserDetails 인터페이스 구현

--- src/main/java/com/uk/shopping/common/auth/PrincipalDetails.java
+++ src/main/java/com/uk/shopping/common/auth/PrincipalDetails.java
@@ -1,2 +1,32 @@
-package com.uk.shopping.common.auth;public class PrincipalDetails {
+package com.uk.shopping.common.auth;
+
+import com.uk.shopping.user.model.User;
+import java.util.Collection;
+import java.util.List;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Data
+public class PrincipalDetails implements UserDetails {
+
+    private User user;
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
 }
